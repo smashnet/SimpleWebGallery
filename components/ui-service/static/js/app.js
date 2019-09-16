2c25db60ce608ac36104199c7d016af58354f5ee
Nicolas Inden <nicolas.inden@innoq.com>
2019-09-16 14:40:00 +0200

    UI-Service: npm update

--- components/ui-service/static/js/app.js
+++ components/ui-service/static/js/app.js
@@ -2,7 +2,7 @@
 	window.global = window;
 }
 
-var commonjsGlobal = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
+var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
 function unwrapExports (x) {
 	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
